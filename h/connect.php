782feee2e2aae2611854a34a85393d668741a432
phatthiya2546 <65010914631@msu.ac.th>
2025-02-04 12:29:16 +0700

    h

--- h/connect.php
+++ h/connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pwd = "12345678Y";
-$db = "shop4631";
+$db = "shop463";
 
 $conn = mysqli_connect($host,$user,$pwd)or die("เชื่อมต่อฐานข้อมูลไม่ได้"); mysqli_select_db ($conn,$db) or die ("เลือกฐานข้อมูลไม่ได้");
 mysqli_query($conn, "set names utf8");
--- i/connect.php
+++ i/connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pwd = "";
-$db = "shopp";
+$pwd = "12345678Y";
+$db = "shop4631";
 
 $conn = mysqli_connect($host,$user,$pwd)or die("เชื่อมต่อฐานข้อมูลไม่ได้"); mysqli_select_db ($conn,$db) or die ("เลือกฐานข้อมูลไม่ได้");
 mysqli_query($conn, "set names utf8");
